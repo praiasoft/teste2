a2107184bfcbf4501b596ee0a74eec9b40311aba
Rodrigo Jesus <rodrigo@praiasoft.com.br>
2020-09-06 22:20:55 -0300

    inicial

---
+++ teste/src/main/java/teste/Operacao.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Operacao {
+
+	public Operacao() {
+	}
+	
+	double soma( double x, double y) {
+		return x+y;
+	}
+	
+	double multiplicacao ( double x, double y ) {
+		return x*y;
+	}
+
+}
